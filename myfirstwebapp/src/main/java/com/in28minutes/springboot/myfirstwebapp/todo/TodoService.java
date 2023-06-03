2f276ce92912ee21de7a5c03d4666c26360b6f42
cpkopp12 <cameronpkopp@gmail.com>
2023-06-03 13:40:46 -0600

    show hardcoded todo list at /list-todos

---
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,21 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	private static List<Todo> todos = new ArrayList<>();
+	static {
+		todos.add(new Todo(1, "in28minutes","learn Aws", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(1, "in28minutes","learn devops", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(1, "in28minutes","learn fullstack", LocalDate.now().plusYears(3), false));
+	}
+	
+	public List<Todo> findByUsername(String username) {
+		return todos;
+	}
+}
---
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title> List Todos Page</title>
+	</head>
+	<body>
+		<div>
+			Welcome to the List Todos page!
+		</div>
+		
+		<div>
+			Your Todos are ${todos}
+		</div>
+		
+	</body>
+</html>
