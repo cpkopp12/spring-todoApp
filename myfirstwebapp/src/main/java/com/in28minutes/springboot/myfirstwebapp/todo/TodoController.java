0af8591dac5301354232443771f75af151b9f168
cpkopp12 <cameronpkopp@gmail.com>
2023-06-04 13:01:15 -0600

    model attributes

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,12 +1,16 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller 
 @SessionAttributes("name")
@@ -33,15 +37,23 @@ public class TodoController {
 	
 	@RequestMapping(value="add-todo", method=RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
-		List<Todo> todos = todoService.findByUsername("in28minutes");
-		model.put("todos", todos);
+		
+		String username = (String)model.get("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
 		
 		return "todoPage";
 	}
 	
 	@RequestMapping(value="add-todo", method=RequestMethod.POST)
-	public String addNewTodo(ModelMap model) {
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
 		
+		if(result.hasErrors()) {
+			return "todoPage";
+		}
+		
+		String username = (String)model.get("name");
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		
 		return "redirect:list-todos";
 	}
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todoPage.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todoPage.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <html>
 	<head>
@@ -9,10 +10,13 @@
 		<div class="container">
 			<h1>Enter Todo Details</h1>
 			
-			<form method="post">
-				Description: <input type="text" name="description" />
+			<form:form method="post" modelAttribute="todo">
+				Description: <form:input type="text" required="required" path="description"/>
+					<form:errors path="description" cssClass="text-warning" />
+				<form:input type="hidden" path="id"/>
+				<form:input type="hidden" path="done"/>
 				<input type="submit" class="btn btn-success"/>
-			</form>
+			</form:form>
 			
 		</div>
 		
